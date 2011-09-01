5ca0a4457e39432d6aadc0b610c8181b23e13e5a
Martin Dybdal <dybber@dybber.dk>
2011-09-01 15:39:54 +0200

    Oops, forgot to commit this a long time ago...

---
+++ tests/unit/HOpenCL/Program_Test.hs
@@ -0,0 +1,77 @@
+module HOpenCL.Program_Test (tests) where
+
+import Foreign.OpenCL.Bindings
+
+import Test.HUnit hiding (Test, test)
+import Test.Framework.Providers.HUnit (testCase)
+import Test.Framework (Test, testGroup, buildTest)
+
+import Control.Monad (forM_, forM, liftM, when)
+
+import Test_Util (oneOfM, void)
+
+--------------------
+--   Test suite   --
+--------------------
+tests = testGroup "Program"
+        [ testCase "Obtain program(s)" testProgram
+        , testCase "Build program(s)" testBuildProgram
+        , testProgramProps
+        ]
+
+kernel = "__kernel void vectorAdd(__global const float * a,"
+      ++                         "__global const float * b, __global float * c) {"
+      ++     "int nIndex = get_global_id(0);"
+      ++     "c[nIndex] = a[nIndex] + b[nIndex];"
+      ++ "};"
+
+testProgramProps = buildTest $ do
+  platforms <- getPlatformIDs
+  devices <- mapM (getDeviceIDs DeviceTypeAll) platforms
+  let pds = zip (map ContextPlatform platforms) devices
+  cs <- forM pds $ \(p, ds) -> createContext ds [p]
+  programs <- forM cs (flip createProgram kernel)
+  let progdevs = [(p,ds) | p <- programs, ds <- devices]
+  let build_opts = ""
+  mapM_ ($build_opts) (map (uncurry buildProgram) progdevs)
+  return $ testGroup "Program properties"
+     [ testCase "programDevice"  $ forM_ (zip programs devices) testProgramDevices
+     , testCase "programContext" $ forM_ (zip programs cs) testProgramContext
+     , testCase "programSource"  $ forM_ programs testProgramSource
+     ]
+
+--------------------
+-- Test functions --
+--------------------
+
+testProgram = do
+  platforms <- getPlatformIDs
+  devices <- mapM (getDeviceIDs DeviceTypeAll) platforms
+  let pds = zip (map ContextPlatform platforms) devices
+  cs <- forM pds $ \(p, ds) -> createContext ds [p]
+  programs <- forM cs (flip createProgram kernel)
+  return ()
+
+testBuildProgram = do
+  platforms <- getPlatformIDs
+  devices <- mapM (getDeviceIDs DeviceTypeAll) platforms
+  let pds = zip (map ContextPlatform platforms) devices
+  cs <- forM pds $ \(p, ds) -> createContext ds [p]
+  programs <- forM cs (flip createProgram kernel)
+  let progdevs = [(p,ds) | p <- programs, ds <- devices]
+  let build_opts = ""
+  mapM_ ($build_opts) (map (uncurry buildProgram) progdevs)
+  return ()
+
+testProgramDevices (program, devices) = do
+  devices' <- programDevices program
+  devices @=? devices'
+
+testProgramContext (program, context) = do
+  context' <- programContext program
+  context @=? context'
+
+testProgramSource program = do
+  source' <- programSource program
+  kernel @=? source'
+
