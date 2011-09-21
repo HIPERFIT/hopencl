35b5e8c24bd7ef452dd6f5505294d115f4fa3db7
Martin Dybdal <dybber@dybber.dk>
2011-09-21 11:53:11 +0200

    Getting HPC to run. It doesn't seem to play that well with c2hs

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,7 @@
+--
+-- This is taken from the POETS-library
+--
+
 import Distribution.Simple
 import Distribution.Simple.LocalBuildInfo (LocalBuildInfo, buildDir)
 import Distribution.PackageDescription (PackageDescription)
@@ -8,26 +12,25 @@ import System.FilePath
 main = defaultMainWithHooks hooks
   where hooks = simpleUserHooks { runTests = runTests' }
 
+hpcReportDir = "hpcreport"
 
 runTests' :: Args -> Bool -> PackageDescription -> LocalBuildInfo -> IO ()
 runTests' _ _ _ lbi = do
   putStrLn "Running HOpenCL tests"
-  print testprog
   system testprog
-  -- putStrLn "computing code coverage ..."
+  -- putStrLn "Computing code coverage"
   -- hpcReport
-  -- putStrLn "generating code coverage reports ..."
+  -- putStrLn "Generating code coverage reports"
   -- hpcMarkup
   return ()
-    where testprog = (buildDir lbi) </> "hopencl_test" </> "hopencl_test"
-          -- hpcReport = system $ "hpc report test"++exclArgs
-          -- hpcMarkup = system $ "hpc markup test --destdir="++hpcReportDir++exclArgs
-          -- excludedModules = ["Main",
-          --                    "Poets.Data.ALaCarte_Gen",
-          --                    "Poets.Data.ALaCarte_Test",
-          --                    "Poets.Data.Type_Gen",
-          --                    "Poets.Data.Type_Test",
-          --                    "Poets.Data.Value_Gen",
-          --                    "Poets.Data.Value_Test",
-          --                    "Poets.Data_Test"]
-          -- exclArgs = concatMap (" --exclude="++) excludedModules
+    where testprog = (buildDir lbi) </> "unit" </> "unit"
+          hpcReport = system $ "hpc report unit"++exclArgs
+          hpcMarkup = system $ "hpc markup unit --destdir="++hpcReportDir++exclArgs
+          excludedModules = ["Test_Util",
+                             "Main",
+                             "HOpenCL.CommandQueue_Test",
+                             "HOpenCL.Context_Test",
+                             "HOpenCL.Platform_Test",
+                             "HOpenCL.Device_Test",
+                             "HOpenCL.Program_Test"]
+          exclArgs = concatMap (" --exclude="++) excludedModules
